35534b7fc1b3940c29c63573a75a6a344c82ac5e
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-15 11:10:48 +0530

    added new amazon test class

---
+++ SeleniumExamples/AmazonTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumExamples
+{
+    internal class AmazonTests
+    {
+        IWebDriver driver;
+        public void InitializeChromeDriver()
+        {
+            driver= new ChromeDriver();
+            driver.Url = "https://www.amazon.com/";
+            driver.Manage().Window.Maximize();
+        }
+        public void InitializeEdgeDriver()
+        {
+            driver = new EdgeDriver();
+            driver.Url = "https://www.amazon.com/";
+            driver.Manage().Window.Maximize();
+        }
+        public void TitleTest()
+        {
+            //Console.WriteLine("Title: "+driver.Title);
+            Assert.AreEqual("Amazon.com. Spend less. Smile more.", driver.Title);
+            Console.WriteLine("Title test - Pass");
+        }
+        public void LogoClickTest()
+        {
+            driver.FindElement(By.Id("nav-logo-sprites")).Click();
+            Assert.AreEqual("Amazon.com. Spend less. Smile more.", driver.Title);
+            Console.WriteLine("Logo click test - Pass");
+
+        }
+        public void SearchProductTest()
+        {
+            driver.FindElement(By.Id("twotabsearchtextbox")).SendKeys("mobiles");
+            Thread.Sleep(5000);
+            driver.FindElement(By.Id("nav-search-submit-button")).Click();
+            Assert.That(("Amazon.com : mobiles".Equals(driver.Title) 
+                && (driver.Url.Contains("mobiles"))));
+            Console.WriteLine("Search Product test - Pass");
+        }
+        public void ReloadHomePageTest()
+        {
+            driver.Navigate().GoToUrl("https://www.amazon.com");
+            Thread.Sleep(3000);
+            Console.WriteLine("Reload home page test - Pass");
+        }
+        public void TodaysDealsTest()
+        {
+            IWebElement todaysdeals = driver.FindElement(By.LinkText("Today's Deals"));
+            if(todaysdeals == null)
+            { 
+                throw new NoSuchElementException("Today's deals link not present");
+            }
+            todaysdeals.Click();
+            Assert.That(driver.FindElement(By.TagName("h1")).Text.Equals("Today's Deals"));
+            Console.WriteLine("Today's deals test - Pass");
+        }
+        public void Destruct()
+        {
+            driver.Close();
+        }
+    }
+}
--- SeleniumExamples/Program.cs
+++ SeleniumExamples/Program.cs
@@ -4,22 +4,59 @@ using OpenQA.Selenium.Chrome;
 using SeleniumExamples;
 
 
-//Assert.That(title == "Gooogle");
-GHPTests gHPTests=new GHPTests();
-//Console.Write("Enter your choice:\n1.Chrome\n2.Edge\n");
-//int choice=Convert.ToInt32(Console.ReadLine());
-//switch(choice)
+////Assert.That(title == "Gooogle");
+//GHPTests gHPTests=new GHPTests();
+////Console.Write("Enter your choice:\n1.Chrome\n2.Edge\n");
+////int choice=Convert.ToInt32(Console.ReadLine());
+////switch(choice)
+////{
+////    case 1:
+////        gHPTests.InitializeChromeDriver();
+////        break;  
+////    case 2:
+////        gHPTests.InitializeEdgeDriver();
+////        break;
+////    default:
+////        Console.WriteLine("Invalid choice");
+////        break;
+////}
+//List<string> drivers = new List<string>();
+//drivers.Add("Chrome");
+////drivers.Add("Edge");
+//foreach (var item in drivers)
 //{
-//    case 1:
-//        gHPTests.InitializeChromeDriver();
-//        break;  
-//    case 2:
-//        gHPTests.InitializeEdgeDriver();
-//        break;
-//    default:
-//        Console.WriteLine("Invalid choice");
-//        break;
+//    switch (item)
+//    {
+//        case "Chrome":
+//            gHPTests.InitializeChromeDriver();
+//            break;
+//        case "Edge":
+//            gHPTests.InitializeEdgeDriver();
+//            break;
+
+//    }
+//    try
+//    {
+//        //gHPTests.TitleTest();
+//        //gHPTests.PageSourceAndURLTest();
+//        //gHPTests.GSTest();
+//        //gHPTests.GmailLinkTest();
+//        //gHPTests.ImagesLinkTest();
+//        //gHPTests.LocalizationTest();
+//        gHPTests.GAppYoutubeTest();
+//    }
+
+//    catch (AssertionException)
+//    {
+//        Console.WriteLine("Fail");
+//    }
+//    gHPTests.Destruct();
 //}
+
+
+
+//*********************AmazonTests************************************
+AmazonTests amazonTests = new AmazonTests();
 List<string> drivers = new List<string>();
 drivers.Add("Chrome");
 //drivers.Add("Edge");
@@ -28,27 +65,29 @@ foreach (var item in drivers)
     switch (item)
     {
         case "Chrome":
-            gHPTests.InitializeChromeDriver();
+            amazonTests.InitializeChromeDriver();
             break;
         case "Edge":
-            gHPTests.InitializeEdgeDriver();
+            amazonTests.InitializeEdgeDriver();
             break;
 
     }
     try
     {
-        //gHPTests.TitleTest();
-        //gHPTests.PageSourceAndURLTest();
-        //gHPTests.GSTest();
-        //gHPTests.GmailLinkTest();
-        //gHPTests.ImagesLinkTest();
-        //gHPTests.LocalizationTest();
-        gHPTests.GAppYoutubeTest();
+        amazonTests.TitleTest();
+        amazonTests.LogoClickTest();
+        amazonTests.SearchProductTest();
+        amazonTests.TodaysDealsTest();
+        
     }
 
     catch (AssertionException)
     {
         Console.WriteLine("Fail");
     }
-    gHPTests.Destruct();
+    catch(NoSuchElementException ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+    amazonTests.Destruct();
 }
