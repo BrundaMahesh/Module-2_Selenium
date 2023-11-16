9e871534bbc4fb61ead23767c5a5835164824e9e
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-16 13:24:53 +0530

    added new class

---
+++ SeleniumNUnitExamples/GHPTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumNUnitExamples
+{
+    [TestFixture]
+    internal class GHPTests:CoreCodes
+    {
+        [Test]
+        public void TitleTest()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Title: " + driver.Title);
+            Console.WriteLine("Title length: " + driver.Title.Length);
+            Assert.AreEqual("Google", driver.Title);
+            Console.WriteLine("Title test - Pass");
+        }
+    }
+}
