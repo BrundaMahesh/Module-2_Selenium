45734e1b2db3ed1caed796407c6ede47472371ed
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-20 16:46:28 +0530

    created new nunit project

---
+++ Assignment-20-11-2023/FlipkartTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_20_11_2023
+{
+    internal class FlipkartTests:CoreCodes
+    {
+        [Test]
+        public void SearchProductTest()
+        {
+
+        }
+    }
+}
