84402dbbf27e4e731db37c6f61420f26904433ef
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-20 18:21:23 +0530

    added new methods

--- Assignment-20-11-2023/FlipkartTests.cs
+++ Assignment-20-11-2023/FlipkartTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +10,34 @@ namespace Assignment_20_11_2023
 {
     internal class FlipkartTests:CoreCodes
     {
+
         [Test]
-        public void SearchProductTest()
+        [Order(1)]
+        public void CloseFirstPop()
         {
+            IWebElement popup= driver.FindElement(By.XPath("//*[@class='_30XB9F']"));
+            popup.Click();
+            Assert.AreEqual(driver.Url,"https://www.flipkart.com/");
 
         }
+
+        [Test]
+        [Order(2)]
+        public void SearchProductTest()
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(50);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            IWebElement searchInput = fluentWait.Until(d => d.FindElement(By.XPath("//input[@name='q']")));
+
+            searchInput.SendKeys("retro");
+            searchInput.SendKeys(Keys.Enter);
+            Assert.That(driver.Url.Contains("retro"));
+
+        }
+        
     }
 }
