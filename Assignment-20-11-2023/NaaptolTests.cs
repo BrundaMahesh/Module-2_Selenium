8c65bd819e4b2266a5494c9504850573c62fb217
BrundaMahesh <brunda.mahesh@ust.com>
2023-11-21 19:08:20 +0530

    modified

--- Assignment-20-11-2023/NaaptolTests.cs
+++ Assignment-20-11-2023/NaaptolTests.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OpenQA.Selenium.Interactions;
+using System.Security.Cryptography;
+using System.Collections;
+using System.IO;
 
 namespace Assignment_20_11_2023
 {
@@ -34,7 +37,36 @@ namespace Assignment_20_11_2023
 
         [Test]
         [Order(2)]
-        public void SelectFifthProduct()
+        [TestCase(5)]
+        public void SelectFifthProductTest(int pid)
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(50);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            string path = "//div[@id='productItem" + pid + "']";
+            Console.WriteLine(path);
+            IWebElement clickFifthProduct = fluentWait.Until(d=>d.FindElement(By.XPath(path)));
+
+            Actions actions = new Actions(driver);
+            Action scroll = () => actions
+            .MoveToElement(clickFifthProduct).Click()
+            .Build()
+            .Perform();
+
+            scroll.Invoke();
+
+            List<string> liswindow = driver.WindowHandles.ToList();
+            driver.SwitchTo().Window(liswindow[1]);
+            Assert.That(driver.Url.Contains("reading-glasses-with-led-lights-lrg4"));
+        }
+
+        [Test]
+        [Order(3)]
+        [TestCase("2.50")]
+        public void AddProductToCartTest(string size)
         {
             DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
             fluentWait.Timeout = TimeSpan.FromSeconds(5);
@@ -42,19 +74,44 @@ namespace Assignment_20_11_2023
             fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
             fluentWait.Message = "Element not found";
            
+            string path = "Black-" + size;
             
-            IWebElement clickFifthProduct = fluentWait.Until(d=>d.FindElement(By.XPath("//div[@id='productItem5' and @pid='12612074']")));
+            Console.WriteLine(path);
+            IWebElement chooseSize = fluentWait.Until(d => d.FindElement(By.LinkText(path)));
 
-            Actions actions = new Actions(driver);
-            Action scroll = () => actions
-            .MoveToElement(clickFifthProduct).Click()
-           .Build()
-            .Perform();
+            chooseSize.Click();
 
-            scroll.Invoke();
-           
-            List<string> liswindow = driver.WindowHandles.ToList();
-            driver.SwitchTo().Window(liswindow[1]);
+            IWebElement clickHereToByButton = driver.FindElement(By.Id("cart-panel-button-0"));
+            Assert.That(driver.Url.Contains("reading-glasses-with-led-lights-lrg4"));
         }
+
+        [Test]
+        [Order(4)]
+        public void ViewShoppingCartTest()
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(5);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(50);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            fluentWait.Message = "Element not found";
+
+            //IWebElement productFound = driver.FindElement(By.LinkText("Reading Glasses with LED Lights (LRG4)"));
+
+            //IWebElement closeButton = fluentWait.Until(d => d.FindElement(By.XPath("/html/body/div[5]/div/a")));
+
+            //IWebElement id = driver.FindElement(By.XPath("//*[text()='My Shopping Cart: ']"));
+            //Assert.AreEqual("My Shopping Cart: At present, you have (1) items. ", id.Text);
+            //closeButton.Click();
+
+            IWebElement closebtn = fluentWait.Until(x => x.FindElement(By.XPath("//a[@class='fancybox-item fancybox-close']")));
+            IWebElement id = driver.FindElement(By.XPath("//*[text()='My Shopping Cart: ']"));
+            Console.WriteLine(id.Text);
+            Assert.That(id.Text == "My Shopping Cart: At present, you have (1) items.");
+            Console.WriteLine("sussess");
+            closebtn.Click();
+            Thread.Sleep(2000);
+
+        }
+
     }
 }
